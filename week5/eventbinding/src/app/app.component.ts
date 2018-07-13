96650740d8422cb94cd32f42fa0cbec49ab8564f
danny <dwaldman#cis.com>
2018-07-12 19:36:14 -0500

    Week 5: add event binding

---
+++ week5/eventbinding/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  onButtonClick = (data: Event) => console.log(`Click event is working with param ${data}`)
+
+}
