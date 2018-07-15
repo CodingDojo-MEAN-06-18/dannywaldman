7d7738cd29f0b5f0d9ec79b6efed5ab44112b45b
danny <dwaldman#cis.com>
2018-07-15 17:08:37 -0500

    Week 5: complete power levels

--- week5/powerlevel/src/app/power/player/player.component.ts
+++ week5/powerlevel/src/app/power/player/player.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
-export class PlayerComponent implements OnInit, OnChanges {
+export class PlayerComponent implements OnChanges {
   @Input() level: number;
-  ngOnChanges(): void {
-    console.log('change');
-  }
+  @Input() type: string;
 
-  ngOnInit = (): void => { }
+  ngOnChanges(): void {
+    switch (this.type.toLowerCase().replace(/\s+/g, '')) {
+      case 'sayian':
+        this.level *= 10;
+        break;
+        case 'supersayian':
+        this.level *= 90;
+        break;
+        case 'supersayiantwo':
+        this.level *= 150;
+        break;
+        case 'supersayianthree':
+        this.level *= 250;
+        break;
+        case 'supersayianfour':
+        this.level *= 500;
+        break;
+    }
+  }
 }
--- week5/powerlevel/src/app/power/power.component.ts
+++ week5/powerlevel/src/app/power/power.component.ts
@@ -1,13 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-power',
   templateUrl: './power.component.html',
   styleUrls: ['./power.component.css']
 })
-export class PowerComponent implements OnInit {
+export class PowerComponent  {
   public level: number = 0;
-  public test: number = 0;
-  onChange = () => {this.test = this.level}
-  ngOnInit = (): void => { }
 }
