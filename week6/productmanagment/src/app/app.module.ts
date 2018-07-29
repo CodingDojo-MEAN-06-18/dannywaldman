9517f40ecce7831ee347274706e6335919ad457e
danny <dwaldman#cis.com>
2018-07-29 17:36:53 -0500

    Week 6: Add product management

---
+++ week6/productmanagment/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+
+import { DataService } from './data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ListComponent,
+    AddComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
